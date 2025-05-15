9f05a8fc4e9a9d9063c043c2e37ea9230ae7f057
danielafoggiatto <danielafoggiatto2014@hotmail.com>
2025-05-15 15:20:04 -0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,42 @@
+﻿//Se
+
+int month = 2;
+string monthName;
+
+if (month == 1)
+    monthName = "January";
+else if (month == 2)
+    monthName = "February";
+else if (month == 3)
+    monthName = "March";
+else
+    monthName = "Unknown";
+
+
+//Escolha
+
+switch (month)
+{
+    case 1:
+        monthName = "January";
+        break;
+    case 2:
+        monthName = "February";
+        break;
+    case 3:
+        monthName = "March";
+        break;
+    default:
+        monthName = "Unknown";
+        break;
+}
+Console.WriteLine($"The month is {monthName}");
+
+/* quando usar if e quando usar swith?
+ if:
+multiplas condições
+quando a condição puder mudar durante o tempo de execução
+
+
+switch:
+meses
---
+++ Program3.cs
@@ -0,0 +1,38 @@
+﻿int num1 = 10;
+int num2 = 3;
+//modulo operator
+int result = num1 % num2;
+//10/3 = 3 remainder -1
+Console.WriteLine(result);
+
+//int num = 0;
+//Console.WriteLine($"Num is {num}");
+//incrementing of int
+//num++;
+//Console.WriteLine($"Num is {num}");
+
+////pre-incrementing
+//Console.WriteLine("Num is {0}", ++num);
+//Console.WriteLine("Num is {0}", num++);
+//Console.WriteLine("Num is {0}", num);
+//// ++variável aumenta em o número em +1 na linha atual.
+//// variável++ aumenta na próxima linha.
+
+////decrementing
+//num--;
+//Console.WriteLine("Num is {0}", num);
+//Console.WriteLine("Num is {0}", --num);
+//Console.WriteLine("Num is {0}", num--);
+//Console.WriteLine("Num is {0}", num);
+
+//num += 30;
+//Console.WriteLine("Num is {0}", num);
+//num -= 10;
+//Console.WriteLine("Num is {0}", num);
+//num *= 10;
+//Console.WriteLine("Num is {0}", num);
+//num /= 20;
+//Console.WriteLine("Num is {0}", num);
+
+
+Console.ReadKey();
