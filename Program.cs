1fe911d6382b6834f41f420c7b3fee32ed82dfc1
danielafoggiatto <danielafoggiatto2014@hotmail.com>
2025-05-15 15:14:01 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,65 +1,57 @@
-﻿using System.Threading.Channels;
+﻿// See https://aka.ms/new-console-template for more information
 
-int num1 = 0;
-int num2 = 0;
-int age = 0;
+string question1 = "What is the capital og Germany";
+string answer1 = "Berlin";
 
-//relational operator < <= > >=
+string question2 = "What is 2 + 2";
+string answer2 = "4";
 
-bool isEqual = num1 == num2;
-bool isNotEqual = num1 != num2;
+string question3 = "What color o you get by mixing blue and yellow";
+string answer3 = "Green";
 
-Console.WriteLine("Please, enter a number:");
+int score = 0;
 
-if (num1 == int.Parse(Console.ReadLine()))
+Console.WriteLine(question1);
+string userAnswer1 = Console.ReadLine();
+if(userAnswer1.Trim().ToLower() == answer1.ToLower())
 {
-    Console.WriteLine("The numbers are equal!");
-
-    Console.WriteLine("Please, enter your age:");
-    age = int.Parse(Console.ReadLine());
-    //a variável age só funciona dentro deste escopo (if)
-    //se criar a variável fora desse escopo, não precisa definir a variável novamente como (int): int age = ...
-    //aqui atribuiu um valor à variável age antes criada, com valor (0)
-    if (age >= 18)
-       
-    {
-        Console.WriteLine("Please, enter your adress, " +
-            "so that we can sen you the price:");
-        string adress = Console.ReadLine();
-    }
-    else
-    {
-        Console.WriteLine("Sorry, you can't get your price due to your age.");
-    }
-        
+    Console.WriteLine("Correct");
+    score = score + 1;
 }
 else
+    Console.WriteLine("Wrong, the correct answer is: " + answer1);
+
+
+Console.WriteLine(question2);
+string userAnswer2 = Console.ReadLine();
+if (userAnswer2.Trim().ToLower() == answer2.ToLower())
 {
-    Console.WriteLine("The numbers aren't equal!");
+    Console.WriteLine("Correct");
+    score = score + 1;
 }
+else
+    Console.WriteLine("Wrong, the correct answer is: " + answer2);
 
 
+Console.WriteLine(question3);
+string userAnswer3 = Console.ReadLine();
+if (userAnswer3.Trim().ToLower() == answer3.ToLower())
+{
+    Console.WriteLine("Correct");
+    score = score + 1;
+    //or score++
+}
+else
+    Console.WriteLine("Wrong, the correct answer is: " + answer3);
 
-    /*
-    //if else elseif
-    int age = 16;
-    bool isWithParents = true;
 
-    if(age >= 13 && isWithParents)
-    {
-        Console.WriteLine("Go party to the club with parents!");
-    }
+Console.WriteLine($"Quiz Completed! Your Final score is: {score}/3");
+if(score == 3)
+    Console.WriteLine("Excellent! You got all the answer right!");
+else if(score > 0)
+    Console.WriteLine("GoodJob, but keep learning");
+else
+    Console.WriteLine("Try again and see if you can get some answer right next time");
 
-    else if(age > 18)
-    {
-        Console.WriteLine("Go party in the club");
-    }
-    else
-    {
-        Console.WriteLine("Go party in the kindergarten!");
-    }
 
-    //O código para de executar assim que encontrar a primeira verdadeira. 
-    */
-
-    Console.ReadKey();
+Console.ReadKey();
